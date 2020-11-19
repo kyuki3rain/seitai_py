8fcb9180669fab19ebef21b81482c85fc62834c0
kyuki3rain <kyuki3rain@gmail.com>
2020-11-18 23:21:41 -0500

    no_eelの複数入力対応

--- no_eel.py
+++ no_eel.py
@@ -5,27 +5,29 @@ import serial_func
  
 length = 50
 port = "COM3"
+data_length = 6
 
 ser = serial_func.start(port)
 
 plt = plot_func.init()
 
 data = serial_func.get_data(ser)
-result = np_func.init(data, length)
+if not 'data_length' in locals():
+    data_length = len(data)
+result = np_func.init(data, length, data_length)
 
 while True:
     try:
         for i in range(0, length):
             data = serial_func.get_data(ser)
             print(data, i)
-            if len(data) != 2:
+            if len(data) != data_length:
                 continue
             
-            result = np_func.set_data(data, *result)
-            plot_func.update(plt, *result)
+            result = np_func.set_data(data, *result, data_length)
+            plot_func.update(plt, *result, data_length)
 
-        a = np_func.check(*result)
-        print(a)
+        f = np_func.check(*result, data_length)
 
     except KeyboardInterrupt:
         ser.close()
