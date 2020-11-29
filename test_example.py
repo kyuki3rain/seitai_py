0b7c2499af71b15d3bb69e0b2c914b2161256eea
kyuki3rain <kyuki3rain@gmail.com>
2020-11-29 12:47:10 +0900

    test_exampleを整備、テスト環境をきれいにした

--- test_example.py
+++ test_example.py
@@ -1,24 +1,40 @@
+import sys
+import statistics
 import numpy as np
 import matplotlib.pyplot as plt
-import statistics
 
 import main
 import enums
 
 color = ["blue", "green", "red", "black", "yellow", "pink", "purple"]
+zz = np.array([])
+
+#  このファイルをコピーしてtest.pyという名前で改変しテストしてください。
+#　そうすれば改変中でもgitに反映されず、気軽にpullできます。
 
 def new_check(t, ys, data_length, bools=None):
-    print("time is ", t)
-    print("ys is ", ys)
-    print("length is ", data_length)
-    print(bools)
+    global zz
+    f = np.array([])
+    s = np.array([])
+    for i in range(0, data_length):
+        p = np.mean(ys[i])
+        f = np.append(f, p > 6)
+        s = np.append(s, np.mean(bools[i]) >= 0.5)
 
-    return bools[::, -1], None # 二つ目の返り値はEELで描画するかどうか。Noneなら前回のfと違う場合のみ描画
+    z = np.mean(np.abs(f - s)) * 100
+    zz = np.append(zz, z)
+
+    if zz.size >= 100:
+        print(np.mean(zz))
+        sys.exit()
+
+    return f, None
+    # 一つ目の返り値は一次元のnparray、二つ目の返り値はEELで描画するかどうかの真偽値（Noneなら前回のfと違う場合のみ描画）
 
 main.app(
     check_function=new_check,
     data_mode=enums.DataMode.TEST,
-    view_mode=enums.ViewMode.SIMPLE,
+    view_mode=enums.ViewMode.TERMINAL,
     import_file_name="tmp/arm2.txt",
     size=10,
     data_length=5,
